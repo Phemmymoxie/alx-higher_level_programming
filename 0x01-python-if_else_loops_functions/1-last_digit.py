940b8ea3437a94b27a22ab3d43b138cd1cf85809
moses ojerinde <gphemmy126@gmail.com>
2024-01-16 22:09:42 +0100

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,18 +3,19 @@ import random
 number = random.randint(-10000, 10000)
 num_str = str(number)
 length = len(num_str)
+lo = "Last digit of"
 if number > 0:
     if int(num_str[length - 1]) > 5:
-        output = f"Last digit of {number} is {num_str[length - 1]} and is greater than 5"
+        output = f"{lo} {number} is {num_str[length - 1]} and is greater than 5"
     elif num_str[length - 1] == '0':
-        output = f"Last digit of {number} is {num_str[length - 1]} and is 0"
+        output = f"{lo} {number} is {num_str[length - 1]} and is 0"
     else:
-        output = f"Last digit of {number} is {num_str[length - 1]} and is less than 6 and not 0"
+        output = f"{lo} {number} is {num_str[length - 1]} and is less than 6 and not 0"
     print(output)
 
 else:
     if num_str[length - 1] == '0':
-        output = f"Last digit of {number} is {num_str[length - 1]} and is 0"
+        output = f"{lo} {number} is {num_str[length - 1]} and is 0"
     else:
-        output = f"Last digit of {number} is -{num_str[length - 1]} and is less than 6 and not 0"
+        output = f"{lo} {number} is -{num_str[length - 1]} and is less than 6 and not 0"
     print(output)

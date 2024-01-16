c0e9d37967583aaf35e40adcbe40890a35ba232c
moses ojerinde <gphemmy126@gmail.com>
2024-01-17 00:49:30 +0100

    fix up

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-def is_lower(c: str):
+def islower(c):
     check = ord(c)
     if 97 < check < 112:
         return True
     else:
         return False
-
-
-print(is_lower('G'))

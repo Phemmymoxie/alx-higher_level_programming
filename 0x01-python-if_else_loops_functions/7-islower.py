bc1bd84dc1dcfb63e3d66272e994c650dfc2358b
moses ojerinde <gphemmy126@gmail.com>
2024-01-17 01:07:45 +0100

    fix up

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(c):
     check = ord(c)
-    if 96 < check < 112:
+    if 96 < check < 122:
         return True
     else:
         return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        chk = ord(char)
+        if 96 < chk < 122:
+            chk -= 32
+            print("{}".format(chr(chk)), end='')
+        else:
+            print(char, end='')
+

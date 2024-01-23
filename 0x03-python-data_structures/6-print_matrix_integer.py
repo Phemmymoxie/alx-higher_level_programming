8b3dc33822a5a02820ca60ba44bb6e6783d91788
moses ojerinde <gphemmy126@gmail.com>
2024-01-23 03:54:39 +0100

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,10 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for li in matrix:
-            len_list = len(li)
-            for element in range(len_list):
-                if element == len_list - 1:
-                    print("{:d}".format(li[element]))
-                else:
-                    print("{:d}".format(li[element]), end=' ')
+            if li:
+                len_list = len(li)
+                for element in range(len_list):
+                    if element == len_list - 1:
+                        print("{:d}".format(li[element]))
+                    else:
+                        print("{:d}".format(li[element]), end=' ')

f1f92d024d8bc1a452a596f87803e12056eaeac3
moses ojerinde <gphemmy126@gmail.com>
2024-01-23 22:41:58 +0100

    max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,8 @@
 
 def max_integer(my_list=[]):
     list_len = len(my_list)
+    if list_len == 0:
+        return None
+
     new_list = sorted(my_list)
-    return int(new_list[list_len - 1])
+    return new_list[list_len - 1]

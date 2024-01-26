18d6e951d5f64ab7627e4ed6b277171a1f967658
moses ojerinde <gphemmy126@gmail.com>
2024-01-26 16:04:19 +0100

    sorted dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    str(a_dictionary.keys())
     normal = sorted(list(a_dictionary.keys()))
     for i in range(len(normal)):
         print("{}: {}".format(normal[i], a_dictionary[normal[i]]))

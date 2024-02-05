7d86b01134ddc676fde42d61b95b3859de7a925f
moses ojerinde <gphemmy126@gmail.com>
2024-02-05 16:43:44 +0100

    python exeptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed_num = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            printed_num += 1
+        print()
+    except IndexError:
+        print()
+        return printed_num
+
+    return printed_num

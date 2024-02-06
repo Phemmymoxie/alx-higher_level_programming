4314e383f314ae30e8fc6254ad3e8c4288cca604
moses ojerinde <gphemmy126@gmail.com>
2024-02-06 14:07:17 +0100

    python classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """ a class Square """
+    def __init__(self, size=0):
+        """ instance of the class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size

e7fdbcd4d750f81bd0a758964e94fd1c82936606
moses ojerinde <gphemmy126@gmail.com>
2024-02-06 13:12:34 +0100

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""class Square"""
+
 
 class Square:
+    """ a class Square """
     def __init__(self, size):
+        """ instance of the class"""
         self._Square__size = size

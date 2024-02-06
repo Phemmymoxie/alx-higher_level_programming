3e9e185950cad3fab8ce1d57b8529fdbac164551
moses ojerinde <gphemmy126@gmail.com>
2024-02-06 12:51:53 +0100

    python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    an empty class Square
+    """
     def square(self):
+        """
+        square
+        :return: none
+        """
         pass

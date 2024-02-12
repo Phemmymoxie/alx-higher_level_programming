fda498baea56379871abe5f7518622685cd91920
moses ojerinde <gphemmy126@gmail.com>
2024-02-12 17:04:02 +0100

    more Classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """ an empty class Rectangle """
+
+    def rectangle(self):
+        """ This function does nothing """
+        pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """ an empty class Rectangle """
+    def __init__(self, width=0, height=0):
+        self._width = width
+        self._height = height
+
+    def rectangle(self):
+        """ This function does nothing """
+        pass
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        if self._height == 0 or self._width == 0:
+            return 0
+        add = self._width + self._height
+        return 2 * add

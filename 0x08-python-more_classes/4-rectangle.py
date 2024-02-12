cc0bcdd5f9d5b1da1d824a25526618c4168cbfb7
moses ojerinde <gphemmy126@gmail.com>
2024-02-12 21:38:08 +0100

    more classes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -53,4 +53,4 @@ class Rectangle:
         return resp_str
 
     def __repr__(self):
-        return f" Rectangle({self.__width}, {self.__height})"
+        return f" Rectangle({str(self.__width)}, {str(self.__height)})"

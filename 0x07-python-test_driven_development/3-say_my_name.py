b67ae41f0d496e20ec5c8880d953816e0411b637
moses ojerinde <gphemmy126@gmail.com>
2024-02-16 00:18:03 +0100

    PTD

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" say_my_name function """
+
 
 def say_my_name(first_name, last_name=""):
+    """ Say My Name Function """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):

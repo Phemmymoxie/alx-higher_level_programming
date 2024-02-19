d9dfa1d95a9b864efd79e946ab25b114e8e2ab90
moses ojerinde <gphemmy126@gmail.com>
2024-02-19 14:09:22 +0100

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@
 
 def inherits_from(obj, a_class):
     """ inherits_from """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class)\
+            and type(obj) != a_class:
         return True
     else:
         return False

74d4a47190b7658b30d9e777e24f71294cb29a5a
moses ojerinde <gphemmy126@gmail.com>
2024-02-19 13:43:24 +0100

    inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" is kind of class function """
+
 
 def is_kind_of_class(obj, a_class):
+    """ is_kind_of_class """
     if isinstance(obj, a_class):
         return True
     else:

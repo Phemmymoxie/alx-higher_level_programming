a198fba5708a73a3687b811a42d8b9a6e91014ed
moses ojerinde <gphemmy126@gmail.com>
2024-02-19 13:52:48 +0100

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" inherits from function """
+
+
+def inherits_from(obj, a_class):
+    """ inherits_from """
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False

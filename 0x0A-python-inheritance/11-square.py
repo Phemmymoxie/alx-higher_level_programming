d5b4cae1c5eb9e899d97de97f692131a09f157ae
moses ojerinde <gphemmy126@gmail.com>
2024-02-22 22:00:02 +0100

    inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Square class that inherits from the Rectangle class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+
+    def __init__(self, size):
+        """ Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ string return """
+        cl_str = "[square] "
+        cl_str += str(self.__size) + "/" + str(self.__size)
+        return cl_str

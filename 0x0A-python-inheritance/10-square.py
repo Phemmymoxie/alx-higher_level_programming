8ef72519ecd16dbe6e945185ae5d1a063801c334
moses ojerinde <gphemmy126@gmail.com>
2024-02-22 01:48:52 +0100

    inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,8 +6,10 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 class Square(Rectangle):
     """ Square class """
     def __init__(self, size):
+        """ the init method """
         self.__size = size
         self.integer_validator("size", self.size)
 
     def area(self):
+        """ area function """
         return self. __size ** 2

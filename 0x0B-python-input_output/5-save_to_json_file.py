2a6b0f3f5357dfd831a76992f7ac5f7f3af08eaa
moses ojerinde <gphemmy126@gmail.com>
2024-02-24 22:27:01 +0100

    json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,7 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ save_to_json_file- save json
+    """ save_to_json_file save json
     object to a file """
     with open(filename, 'w') as content:
         content.write(json.dumps(my_obj))

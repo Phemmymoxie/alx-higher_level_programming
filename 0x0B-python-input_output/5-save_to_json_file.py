685d87c742892c37d3b4e22d13d54655e5d4ae05
moses ojerinde <gphemmy126@gmail.com>
2024-02-24 22:30:20 +0100

    json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bim/python3
-""" save to json file """
+"""save to json file """
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ save_to_json_file save json
+    """save_to_json_file save json
     object to a file """
-    with open(filename, 'w') as content:
-        content.write(json.dumps(my_obj))
+    with open(filename, 'w', encoding='utf-8') as content:
+        json.dump(my_obj, content)
